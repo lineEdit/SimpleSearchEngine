94f559a852ff80e5b6fa67bd43d1d7891889cd26
lineedit <lineedit@gmail.com>
2020-03-30 21:46:07 +0500

    Stage 3, testing menu.

--- Simple Search Engine/task/src/search/Main.java
+++ Simple Search Engine/task/src/search/Main.java
@@ -16,26 +16,43 @@ public class Main {
             list.add(scanner.nextLine());
             num--;
         }
-        System.out.println("\nEnter the number of search queries: ");
-        num = scanner.nextInt();
-        while (num >= 0) {
-            System.out.println("\nEnter data to search people: ");
-            String find = scanner.next();
-            List<String> result = new ArrayList<>();
-            for (String item : list) {
-                if (item.toLowerCase().replaceAll(" ", "").contains(find.toLowerCase())) {
-                    result.add(item);
-                }
+        while (true) {
+            System.out.println(
+                    "=== Menu ===\n" +
+                    "1. Find a person\n" +
+                    "2. Print all people\n" +
+                    "0. Exit");
+            int menu = scanner.nextInt();
+            switch (menu) {
+                case 0:
+                    System.out.println("Bye!");
+                    return;
+                case 1:
+                    System.out.println("Enter a name or email to search all suitable people.");
+                    String find = scanner.next();
+                    List<String> result = new ArrayList<>();
+                    for (String item : list) {
+                        if (item.toLowerCase().replaceAll(" ", "").contains(find.toLowerCase())) {
+                            result.add(item);
+                        }
+                    }
+                    if (result.size() > 0) {
+                        for (String item : result) {
+                            System.out.println(item);
+                        }
+                    } else {
+                        System.out.println("\nNo matching people found.");
+                    }
+                    break;
+                case 2:
+                    System.out.println("=== List of people ===");
+                    for (String item : list) {
+                        System.out.println(item);
+                    }
+                    break;
+                default:
+                    System.out.println("Incorrect option! Try again.");
             }
-            if (result.size() > 0) {
-                System.out.println("\nFound people: ");
-                for (String item : result) {
-                    System.out.println(item);
-                }
-            } else {
-                System.out.println("\nNo matching people found.");
-            }
-            num--;
         }
     }
 }
